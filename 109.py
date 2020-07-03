d1f8ddd5ec4acb512dde7ed36b857828f374ea73
deimelperez <deimelperez@gmail.com>
2020-07-03 15:12:11 -0400

    Challenge 137

--- 109.py
+++ 109.py
@@ -5,20 +5,20 @@ ch = int(input('Select 1, 2 or 3: '))
 while ch != 1 and ch != 2 and ch != 3:
     ch = int(input('Please, select 1, 2 or 3: '))
 if ch == 1:
-    file = open('109 Subjects.txt','w')
+    file = open('109 Subjects.txt', 'w')
     subject = input("Enter a subject: ")
     file.write(subject + '\n')
     file.close()
 elif ch == 2:
-    file = open('109 Subjects.txt','r')
+    file = open('109 Subjects.txt', 'r')
     print(file.read())
     file.close()
 else:
-    file = open('109 Subjects.txt','a')
+    file = open('109 Subjects.txt', 'a')
     subject = input("Enter a subject: ")
     file.write(subject + '\n')
     file.close()
-    file = open('109 Subjects.txt','r')
+    file = open('109 Subjects.txt', 'r')
     print(file.read())
     file.close()
 
